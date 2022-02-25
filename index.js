e3d083ae4bba3ae108f1f4f8f1abf31e9f2a3c61
Abega <ebenezermelkamu@gmail.com>
2022-02-25 08:16:28 +0300

    Add Procfile

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get("/tiktok", (req, res) => {
     res.sendStatus(200)
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
