ef07af701147cf5fb41c64710f4d4dd729b678f6
Abega <ebenezermelkamu@gmail.com>
2022-02-25 08:21:53 +0300

    Change port

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get("/tiktok", (req, res) => {
     res.sendStatus(200)
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
