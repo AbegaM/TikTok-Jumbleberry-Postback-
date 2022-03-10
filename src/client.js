db42bbb5c7b659e4e8c4c0c4ab810561e2832c9e
Abega <ebenezermelkamu@gmail.com>
2022-03-10 19:02:49 +0300

    Change access token

--- src/client.js
+++ src/client.js
@@ -1,7 +1,8 @@
 const axios = require("axios").default;
 
 const BASE_URL = "https://business-api.tiktok.com/open_api/v1.2/pixel/track/";
-const ACCESS_TOKEN = "1a1e10a7f3df1c2ef98c4f5a2051ef9cbaeb7ccb";
+//const ACCESS_TOKEN = "1a1e10a7f3df1c2ef98c4f5a2051ef9cbaeb7ccb"; //for JB Tik Tok S2S Pixel 
+const ACCESS_TOKEN = "8730294af6b5da6ded3099a46f83201e8d0e78a4"
 const CONFIG = {};
 
 const postData = async (body) => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const client = require("./client");
 const app = express();
 
 app.get("/", async (req, res) => {
-  //const data = { pixel_code: "C67BFNGQCDCUAMIVI4R0", event: "ViewContent" };
+  // const data = { pixel_code: "C8L1BDCLGSOAGACE91I0", event: "ViewContent" };
   // const response = await client.postData(data);
   // console.log(response);
   res.send({ message: "main route working" });
@@ -17,10 +17,25 @@ app.post("/tiktok", async (req, res) => {
   res.sendStatus(200);
 });
 
+app.get("/testn", async (req, res) => {
+  // const data = {
+  //   pixel_code: "C88AJ03U4UMEF5JVB7IG",
+  //   event: "ViewContent",
+  //   // context: { ad: { callback: "C67BFNGQCDCUAMIVI4R0" } },
+  //   // page: {
+  //   //   url: "https://www.onnit.com/alpha-brain-offer-jmbs?utm_medium=jumbleberry&utm_campaign=affiliate&utm_content=alpha-brain-offer-jmb&utm_source=388083&jmb_cid=hY29mqA2lYvaalb3Tbo1VpoJ-dao7cmKfh1oOdGQj8LHHfP9wnSBomiCL_b-9R5A%2F%2F%2F&ttclid=__CLICKID__",
+  //   // },
+  // };
+  const data = { pixel_code: "C8L1BDCLGSOAGACE91I0", event: "ViewContent" };
+  const response = await client.postData(data);
+  console.log(response);
+  res.send({ message: "main route working" });
+});
+
 app.get("/tiktok", async (req, res) => {
   console.log("GET ROUTE FOR TIKTOK");
   console.log(req.query);
-  const data = {pixel_code: req.query.ttclid, event: req.query.event}
+  const data = { pixel_code: req.query.ttclid, event: req.query.event };
   const response = await client.postData(data);
   console.log(response);
   res.sendStatus(200);
